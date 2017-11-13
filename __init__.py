11f63d157b2bee51a67747be441425c19b1f82e6
Jefferson Campos <foguinho.peruca@gmail.com>
2017-11-12 23:47:52 -0200

    GNUCash INterface
    Stub file.

--- __init__.py
+++ __init__.py
@@ -7,6 +7,10 @@ import logging
 # ofxparse 0.14+
 from ofxparse import OfxParser
 
+# GNUCash interface
+# import gnucash_interface
+from gnucash_interface import write_to_gnucash_file
+
 # gather the uuid frm kernel...
 # uuid = open('/proc/sys/kernel/random/uuid', 'r')
 # print str(uuid.uuid4())
@@ -16,24 +20,29 @@ def read_nubank(ofx_file):
 
     ofx = OfxParser.parse(file(ofx_file))
 
-    for transaction in ofx.account.statement.transactions:
-        # print dir(transaction)
-        print "amount: %s" % (transaction.amount)
-        print "checknum: %s" % (transaction.checknum)
-        print "date: %s" % (transaction.date)
-        print "id: %s" % (transaction.id)
-        print "mcc: %s" % (transaction.mcc)
-        print "memo: %s" % (transaction.memo)
-        print "payee: %s" % (transaction.payee)
-        print "sic: %s" % (transaction.sic)
-        print "type: %s" % (transaction.type)
-        print "------------------------------------------------------------"
+    # for transaction in ofx.account.statement.transactions:
+    #     print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    #     # print dir(transaction)
+    #     print "amount: %s" % (transaction.amount)
+    #     print "checknum: %s" % (transaction.checknum)
+    #     print "date: %s" % (transaction.date)
+    #     print "id: %s" % (transaction.id)
+    #     print "mcc: %s" % (transaction.mcc)
+    #     print "memo: %s" % (transaction.memo.encode('iso-8859-1'))
+    #     print "payee: %s" % (transaction.payee)
+    #     print "sic: %s" % (transaction.sic)
+    #     print "type: %s" % (transaction.type)
+    #     print "------------------------------------------------------------"
+    #     # create_gnucash_tansaction()
+        
+
+        
 
 def get_args():
     parser = argparse.ArgumentParser(description = "GNUCash utility to fix xml file and import custom data.")
     parser.add_argument("-q", "--quiet", action='store_true', help="Set *NO* verbose logging i.e.: loglevel = logging.WARN")
     parser.add_argument("-v", "--verbose", action='store_true', help="Set *VERBOSE* logging i.e.: loglevel = logging.DEBUG")
-    # parser.add_argument("gnucash_file", help="GNUCash xml file")
+    parser.add_argument("gnucash_file", help="GNUCash xml file")
     args = parser.parse_args()
 
     return args
@@ -48,7 +57,18 @@ def main():
         loglevel = logging.INFO
 
     logging.basicConfig(level = loglevel)
-    read_nubank('nubank.ofx')
+
+    if args.gnucash_file is None:
+        file_path = nubank.ofx
+    else:
+        file_path = args.gnucash_file
+
+        
+    read_nubank(file_path)
+    # gnucash_interface.write_to_gnucash_file()
+
+    write_to_gnucash_file()
+    
 
 if __name__ == "__main__":
     main()
---
+++ gnucash_interface.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Meant to be reuseable interface with gnucash
+
+'''
+# from gnucash import Session, Transaction, Split, GncNumeric
+
+def create_gnucash_tansaction():
+    print "TODO stub: Creating a GNUCash transaction"
+    
+def write_to_gnucash_file():
+    print "TODO stub: Writing to gNUCash file"
