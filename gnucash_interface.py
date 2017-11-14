e6a5b882490e4c7589911f488dd999f21aab4130
Jefferson Campos <foguinho.peruca@gmail.com>
2017-11-14 18:51:51 -0200

    GNUCash Transaction.
    Basic skeleton.

--- gnucash_interface.py
+++ gnucash_interface.py
@@ -5,10 +5,46 @@
 Meant to be reuseable interface with gnucash
 
 '''
-# from gnucash import Session, Transaction, Split, GncNumeric
 
-def create_gnucash_tansaction():
+import datetime
+from gnucash import Session, Transaction, Split, GncNumeric
+
+def create_gnucash_tansaction(book, item, curr):
     print "TODO stub: Creating a GNUCash transaction"
+    # TODO get correct accounts
+    acc_from = ""
+    acc_to = ""
+    amount = 0                  # TODO calc amount
+
+    if curr is None:
+        curr = "BRL"
+
+    if item is None:
+        item = ""               # TODO throw an exception
+
+    if book is None:
+        book = ""               # TODO throw an exception
+
+    tx = Transaction(book)
+    tx.BeginEdit()
+    tx.SetCurrency(curr)
+    tx.SetDateEnteredTS(datetime.datetime.now())
+    tx.SetDatePostedTS(item.date)
+    tx.SetDescription(item.memo)
+
+    split_from = Split(book)
+    split_from.SetParent(tx)
+    split_from.SetAccount(acc_from)
+    split_from.SetValue(0)      # TODO set correct value
+    split_from.SetAmount(0)     # TODO set correct amount
+
+    split_to = Split(book)
+    split_to.SetParent(tx)
+    split_to.SetAccount(acc_to)
+    split_to.SetValue(0)        # TODO set correct value
+    split_to.SetAmount(0)       # TODO set correct amount
+
+    tx.CommitEdit()
     
 def write_to_gnucash_file():
-    print "TODO stub: Writing to gNUCash file"
+    print "TODO stub: Writing to GNUCash file"
