b83a0cb296799394e8a2589189528162dafee013
Jefferson Campos <foguinho.peruca@gmail.com>
2017-11-15 01:00:29 -0200

    Structure
    Added more pythonic

---
+++ gnucash_importer.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import uuid
+import argparse
+import logging
+
+# ofxparse 0.14+
+from ofxparse import OfxParser
+
+# GNUCash interface
+import gnucash_interface
+# from gnucash_interface import write_to_gnucash_file
+
+def read_nubank(ofx_file):
+    print 'Reading Nubank data from .ofx!!'
+
+    ofx = OfxParser.parse(file(ofx_file))
+
+    # for transaction in ofx.account.statement.transactions:
+    #     print "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    #     # print dir(transaction)
+    #     print "amount: %s" % (transaction.amount)
+    #     print "checknum: %s" % (transaction.checknum)
+    #     print "date: %s" % (transaction.date)
+    #     print "id: %s" % (transaction.id)
+    #     print "mcc: %s" % (transaction.mcc)
+    #     print "memo: %s" % (transaction.memo.encode('iso-8859-1'))
+    #     print "payee: %s" % (transaction.payee)
+    #     print "sic: %s" % (transaction.sic)
+    #     print "type: %s" % (transaction.type)
+    #     print "------------------------------------------------------------"
+    #     # create_gnucash_tansaction()
+        
+
+        
+
+def get_args():
+    parser = argparse.ArgumentParser(description = "GNUCash utility to fix xml file and import custom data.")
+    parser.add_argument("-q", "--quiet", action='store_true', help="Set *NO* verbose logging i.e.: loglevel = logging.WARN")
+    parser.add_argument("-v", "--verbose", action='store_true', help="Set *VERBOSE* logging i.e.: loglevel = logging.DEBUG")
+    parser.add_argument("gnucash_file", help="GNUCash xml file")
+    args = parser.parse_args()
+
+    return args
+
+def main():
+    args = get_args()
+    if args.verbose:
+        loglevel = logging.DEBUG
+    elif args.quiet:
+        loglevel = logging.WARN
+    else:
+        loglevel = logging.INFO
+
+    logging.basicConfig(level = loglevel)
+
+    if args.gnucash_file is None:
+        file_path = nubank.ofx
+    else:
+        file_path = args.gnucash_file
+
+        
+    read_nubank(file_path)
+    # gnucash_interface.write_to_gnucash_file()
+
+    gnucash_interface.stub()
+    
+
+if __name__ == "__main__":
+    main()
--- gnucash_interface.py
+++ gnucash_interface.py
@@ -3,18 +3,22 @@
 
 '''
 Meant to be reuseable interface with gnucash
-
 '''
 
 import datetime
+from Decimal import decimal
 from gnucash import Session, Transaction, Split, GncNumeric
+
+def get_currency(book, curr = default_currency):
+    commod_tab = book.get_table()
+    currency = commod_tab.lookup('ISO4217', curr)
 
 def create_gnucash_tansaction(book, item, curr):
     print "TODO stub: Creating a GNUCash transaction"
     # TODO get correct accounts
     acc_from = ""
     acc_to = ""
-    amount = 0                  # TODO calc amount
+    amount = int(Decimal(item.split_amount.replace(',', '.')) * curr.get_fraction())
 
     if curr is None:
         curr = "BRL"
@@ -35,16 +39,19 @@ def create_gnucash_tansaction(book, item, curr):
     split_from = Split(book)
     split_from.SetParent(tx)
     split_from.SetAccount(acc_from)
-    split_from.SetValue(0)      # TODO set correct value
-    split_from.SetAmount(0)     # TODO set correct amount
+    split_from.SetValue(GncNumeric(amount, curr.get_fraction()))
+    split_from.SetAmount(GncNumeric(amount, curr.get_fraction()))
 
     split_to = Split(book)
     split_to.SetParent(tx)
     split_to.SetAccount(acc_to)
-    split_to.SetValue(0)        # TODO set correct value
-    split_to.SetAmount(0)       # TODO set correct amount
+    split_to.SetValue(GncNumeric(amount, curr.get_fraction()))
+    split_to.SetAmount(GncNumeric(amount, curr.get_fraction()))
 
     tx.CommitEdit()
     
 def write_to_gnucash_file():
     print "TODO stub: Writing to GNUCash file"
+
+def stub():
+    print "Just a stub method..."
