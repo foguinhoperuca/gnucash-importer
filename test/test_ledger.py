cd311a624deb09f24340bc70c14c57d17ea9ce1a
Jefferson Campos <foguinho.peruca@gmail.com>
2018-11-20 19:37:03 -0200

    Running Tests
    Working with Ledger.write().

--- test/test_ledger.py
+++ test/test_ledger.py
@@ -1,20 +1,22 @@
 # FIXME can't import classes to test....
 # import sys
 # sys.path.append('../gnucash_importer')
-# import gnucash_importer
+import gnucash_importer
 from gnucash_importer.util import Util
-# from gnucash_importer.account import Nubank # FIXME not working... :'(
+from gnucash_importer.account import Nubank
+from gnucash_importer.ledger import Ledger
 import unittest
 
 class TestLedger(unittest.TestCase):
     def setUp(self):
         self.util = Util()
-        # self.account = Nubank('example/local/nubank-2016-10.ofx')
-    #     self.ledger = Ledger(self.account, self.util.DEFAULT_CURRENCY, False, self.util.DEFAULT_GNUCASH_FILE)
+        self.account = Nubank('example/local/nubank-2016-10.ofx')
+        self.ledger = Ledger(self.account, self.util.DEFAULT_CURRENCY, False, self.util.DEFAULT_GNUCASH_FILE)
 
     def test_get_gnucash_currency(self):
         # def get_gnucash_currency(self, book, curr = 'BRL'):
         self.assertTrue(True)
+        self.ledger.write()
 
 if __name__ == '__main__':
     unittest.main()
